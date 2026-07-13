eab6d0a6af7b4419afd89aa43a640ef9665b6e59
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-07-13 15:45:54 +0530

    feat: add login form (exercise 5.1)

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -1,22 +1,75 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
 
-  return (
-    <div>
+  const loginForm = () => (
+    <>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input type="text" value={password} onChange={({ target }) => setPassword(target.value)} />
+          </label>
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </>
+
+  )
+
+  const userBlogsInfo = () => (
+    <>
       <h2>blogs</h2>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </>
+  )
+
+  const handleLogin = async event => {
+    event.preventDefault()
+    //console.log('logging in with', username, password)
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      console.log('wrong credentials')
+    }
+  }
+
+  return (
+    <div>
+      {!user && loginForm()}
+      {user && (
+      <div>
+        <p>{user.name} logged in</p>
+        {userBlogsInfo()}
+      </div>
+    )}
     </div>
   )
 }
