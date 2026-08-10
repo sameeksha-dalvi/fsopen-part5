494b8582f40e3794701baa2660d30499582e3105
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-08-10 16:41:55 +0530

    feat: add blog detail visibility toggle(exercise 5.7)

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [visible, setVisible] = useState(false)
+
+  const showBlogDetail = { display: visible ? '' : 'none' }
+
+  const toggleBlogVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={toggleBlogVisibility}> {visible ? 'hide' : 'view'}</button>
+      </div>
+      <div style={showBlogDetail}>
+        {blog.url}
+        <div>
+          {blog.likes}
+          <button>like</button>
+        </div>
+
+        {blog.author}
+      </div>
+
+    </div>
+  )
+}
 
 export default Blog
