142adcfb8f554b973b9f7e6a0a64d8f6dc9be158
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-08-22 19:05:31 +0530

    test: blog details visible after clicking view (exercise 5.14)

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,29 +1,51 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders title and author but not url or likes by deafult', () => {
-  const blog = {
-    title: 'Testing React apps',
-        author: 'Author Name',
-        url: 'https://example.com',
-        likes: 10,
-        user: {
-            id: '123',
-            username: 'sameeksha',
-            name: 'Sameeksha'
-        }
-    }
+let blog
+let container
 
-    const { container } = render(
-        <Blog
-            blog={blog}
-        />
-    )
+beforeEach(() => {
+  blog = {
+    title: 'Testing React apps',
+    author: 'Author Name',
+    url: 'https://example.com',
+    likes: 10,
+    user: {
+      id: '123',
+      username: 'sameeksha',
+      name: 'Sameeksha'
+    }
+  }
+
+  const rendered = render(
+    <Blog
+      blog={blog}
+    />
+  )
+
+  container = rendered.container
+})
+
+test('renders title and author but not url or likes by deafult', () => {
 
     const blogTitleAuthor = container.querySelector('.blog-title-author')
     const blogDetails = container.querySelector('.blog-details')
 
     expect(blogTitleAuthor).toBeVisible()
     expect(blogDetails).not.toBeVisible()
+
+})
+
+test('after clicking button, children are displayed', async () => {
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+
+    const blogDetails = container.querySelector('.blog-details')
+
+    expect(blogDetails).toBeVisible()
+    expect(blogDetails).toHaveTextContent('https://example.com')
+    expect(blogDetails).toHaveTextContent('10')
 
 })
