4ba512e152dc0305c4691850685160e33ea90f45
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-08-25 18:19:54 +0530

    add E2E tests for successful and failed login (exercise 5.18)

--- bloglist-e2e/tests/blog_app.spec.js
+++ bloglist-e2e/tests/blog_app.spec.js
@@ -1,12 +1,43 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-    beforeEach(async ({ page }) => {
+    beforeEach(async ({ page, request }) => {
+
+        await request.post('http://localhost:3003/api/testing/reset')
+        await request.post('http://localhost:3003/api/users', {
+            data: {
+                name: 'Sam D',
+                username: 'sameeksha',
+                password: 'mypassword'
+            }
+        })
+
         await page.goto('http://localhost:5173')
     })
 
     test('Login form is shown', async ({ page }) => {
         const locator = page.getByText('Log in to Blogs App')
         await expect(locator).toBeVisible()
+    })
+
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            await page.getByRole('textbox').first().fill('sameeksha')
+            await page.getByRole('textbox').last().fill('mypassword')
+            await page.getByRole('button', { name: 'login' }).click()
+
+            await expect(page.getByText('Sam D logged in')).toBeVisible()
+        })
+
+        test('fails with wrong credentials', async ({ page }) => {
+            await page.getByRole('textbox').first().fill('sameeksha')
+            await page.getByRole('textbox').last().fill('wrongpassword')
+            await page.getByRole('button', { name: 'login' }).click()
+
+            const errorDiv = page.locator('.error')
+            await expect(errorDiv).toContainText('wrong credentials')
+            await expect(errorDiv).toHaveCSS('border-style', 'solid')
+            await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+        })
     })
 })
